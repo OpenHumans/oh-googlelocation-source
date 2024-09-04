48317c03c0ce7c5c2a1664881023939356983ea1
Bastian Greshake Tzovaras <bgreshake@googlemail.com>
2024-09-04 13:16:59 +0100

    update deps

--- main/urls.py
+++ main/urls.py
@@ -1,15 +1,15 @@
-from django.conf.urls import url
+from django.urls import re_path
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^complete/?$', views.complete, name='complete'),
-    url(r'^logout/?$', views.logout_user, name='logout'),
-    url(r'^overview/?$', views.overview, name='overview'),
-    url(r'^upload_simple/?$', views.upload_old, name='upload_old'),
-    url(r'^about/?$', views.about, name='about'),
-    url(r'^list/?$', views.list_files, name='list'),
-    url(r'^delete/(?P<file_id>\w+)/?$', views.delete_file, name='delete'),
-    url(r'^trigger_processing/?$', views.trigger, name='trigger'),
+    re_path(r'^$', views.index, name='index'),
+    re_path(r'^complete/?$', views.complete, name='complete'),
+    re_path(r'^logout/?$', views.logout_user, name='logout'),
+    re_path(r'^overview/?$', views.overview, name='overview'),
+    re_path(r'^upload_simple/?$', views.upload_old, name='upload_old'),
+    re_path(r'^about/?$', views.about, name='about'),
+    re_path(r'^list/?$', views.list_files, name='list'),
+    re_path(r'^delete/(?P<file_id>\w+)/?$', views.delete_file, name='delete'),
+    re_path(r'^trigger_processing/?$', views.trigger, name='trigger'),
 ]
--- project_admin/urls.py
+++ project_admin/urls.py
@@ -1,21 +1,21 @@
-from django.conf.urls import url
+from django.urls import re_path
 
 from . import views
 
 app_name = 'project-admin'
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
-    url(r'^config-general-settings/?$',
+    re_path(r'^$', views.home, name='home'),
+    re_path(r'^config-general-settings/?$',
         views.config_general_settings, name='config-general-settings'),
-    url(r'^config-oh-settings/?$',
+    re_path(r'^config-oh-settings/?$',
         views.config_oh_settings, name='config-oh-settings'),
-    url(r'^config-file-settings/?$',
+    re_path(r'^config-file-settings/?$',
         views.config_file_settings, name='config-file-settings'),
-    url(r'^config-homepage-text/?$',
+    re_path(r'^config-homepage-text/?$',
         views.config_homepage_text, name='config-homepage-text'),
-    url(r'^login/?$', views.admin_login, name='login'),
-    url(r'^add-file/?$', views.add_file, name='add-file'),
-    url(r'^delete-file/(?P<file_id>\w+)/?$', views.delete_file,
+    re_path(r'^login/?$', views.admin_login, name='login'),
+    re_path(r'^add-file/?$', views.add_file, name='add-file'),
+    re_path(r'^delete-file/(?P<file_id>\w+)/?$', views.delete_file,
         name='delete-file'),
 ]
